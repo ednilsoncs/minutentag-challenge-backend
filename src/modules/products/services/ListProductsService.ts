ae2535eb9a9e3d45c63a64803f2161e3ee2ba544
ednilsoncs <ednilsonsantos43@gmail.com>
2024-07-28 10:03:58 -0300

    chore: add env example and add price to items

--- src/modules/products/services/ListProductsService.ts
+++ src/modules/products/services/ListProductsService.ts
@@ -1,16 +1,37 @@
 
+import ListProductInventoryService from '../../stocks/services/ListProductInventoryService'
 import productsDataBase from '../../../shared/db/products'
 
 class ListProductsService {
   
   public async execute(
   ) {   
-  const formattedList = productsDataBase.map(product=> {
-    return {
-      ...product,
-      image: `${process.env.FILE_URL}${product.image}`
+  const listProductInventoryService = new ListProductInventoryService()
+
+
+  const formattedList = []
+
+  for await (const product of productsDataBase){
+    let price = 0
+
+    for await (const item of product.skus){ 
+      const inventoryItem = await listProductInventoryService.execute({sku: item.code})
+      
+      if(inventoryItem.stock > 0 && ( price === 0 || inventoryItem.price < price))
+        console.log(inventoryItem)
+        price = inventoryItem.price
     }
-  })
+     
+
+    console.log(price)
+   
+
+    formattedList.push({
+      ...product,
+      image: `${process.env.FILE_URL}${product.image}`,
+      price: price
+    })
+  }
  
 
    return formattedList
